828d109341ec0c7dee616011871a643b6b42de82
Brad <BradPatras@users.noreply.github.com>
2025-01-28 21:17:54 -0600

    Cleaned up main screen attempting to fix bottom tab crash, updated dependency versions

--- app/src/main/java/io/github/bradpatras/justworkout/ui/MainScreen.kt
+++ app/src/main/java/io/github/bradpatras/justworkout/ui/MainScreen.kt
@@ -1,5 +1,5 @@
 package io.github.bradpatras.justworkout.ui
-
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -12,15 +12,31 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.generated.NavGraphs
 import com.ramcosta.composedestinations.generated.destinations.ExerciseListScreenDestination
 import com.ramcosta.composedestinations.generated.destinations.WorkoutListScreenDestination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.rememberNavHostEngine
+import com.ramcosta.composedestinations.spec.DestinationSpec
+import com.ramcosta.composedestinations.spec.DirectionDestinationSpec
 import com.ramcosta.composedestinations.utils.currentDestinationAsState
+import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 import com.ramcosta.composedestinations.utils.startDestination
+import io.github.bradpatras.justworkout.R
+
+enum class BottomBarDestination(
+    val direction: DirectionDestinationSpec,
+    val icon: ImageVector,
+    @StringRes val label: Int
+) {
+    Exercises(ExerciseListScreenDestination, Icons.AutoMirrored.Filled.List, R.string.exercises),
+    Workouts(WorkoutListScreenDestination, Icons.Default.DateRange, R.string.workouts),
+}
 
 @Composable
 fun MainScreen() {
@@ -45,41 +61,24 @@ fun MainScreen() {
 
 @Composable
 fun BottomBar(navController: NavController) {
-    val currentDestination = navController.currentDestinationAsState().value ?: NavGraphs.root.startDestination
+    val currentDestination: DestinationSpec = navController.currentDestinationAsState().value
+        ?: NavGraphs.root.startDestination
     BottomAppBar {
-        NavigationBarItem(
-            selected = ExerciseListScreenDestination == currentDestination,
-            onClick = {
-                navController.navigate(
-                    ExerciseListScreenDestination
-                ) {
-                    launchSingleTop = true
-                    restoreState = true
+        BottomBarDestination.entries.forEach { destination ->
+            NavigationBarItem(
+                selected = currentDestination == destination.direction,
+                onClick = {
+                    navController.navigate(destination.direction) {
+                        launchSingleTop = true
+                    }
+                },
+                icon = {
+                    Icon(destination.icon, stringResource(destination.label))
+                },
+                label = {
+                    Text(stringResource(destination.label))
                 }
-            },
-            icon = {
-                Icon(Icons.AutoMirrored.Filled.List, "exercise list tab")
-            },
-            label = {
-                Text("Exercises")
-            }
-        )
-        NavigationBarItem(
-            selected = WorkoutListScreenDestination == currentDestination,
-            onClick = {
-                navController.navigate(
-                    WorkoutListScreenDestination
-                ) {
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            },
-            icon = {
-                Icon(Icons.Default.DateRange, "workouts list tab")
-            },
-            label = {
-                Text("Workouts")
-            }
-        )
+            )
+        }
     }
 }
